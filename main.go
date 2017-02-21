c480b069b03e2dc085206ef08219b5b194b1eb54
Cyrus Hanlon <cyrushanlon@hotmail.com>
2017-02-21 19:20:45 +0000

    Initial server

--- main.go
+++ main.go
@@ -1,96 +1,23 @@
 package main
 
 import (
+	"io"
 	"log"
-	"net"
-	"sync"
-	"strings"
+	"net/http"
 )
 
-var (
-//	ClientIPs = []string{}
-	wg sync.WaitGroup
-)
-
-func main() {
-
-	for {
-
-		ln, err := net.Listen("tcp4", ":6263")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		log.Println("Listening to localhost:6263")
-
-		for {
-			log.Println("Waiting to accept a connection")
-			conn, err := ln.Accept()
-			if err != nil {
-				log.Println(err)
-			}
-
-			newAdd := strings.Split(conn.RemoteAddr().String(), ":")[0]
-
-			wg.Add(1)
-			log.Println("serving", newAdd)
-			go ServeConnection(conn, newAdd)
-		}
+func hello(w http.ResponseWriter, r *http.Request) {
+	_, err := io.WriteString(w, "Hello world!")
+	if err != nil {
+		log.Println(err)
 	}
 }
 
-func ServeConnection(conn net.Conn, addr string) {
-	//serve the connection
-	for {
-		buffer := make([]byte, 4096)
-		n, err := conn.Read(buffer)
-		if err != nil {
-			log.Println(err)
-			wg.Done()
-			break
-		}
+func main() {
+	http.HandleFunc("/", hello)
 
-		if n == 0 {
-			continue
-		} else {
-
-			log.Println("Setting Clipboard on", addr)
-
-			outconn, err := net.Dial("tcp4", addr + ":6264")
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			n, err := outconn.Write(buffer)
-			if err != nil {
-				log.Println("n:", n, err)
-			}
-			outconn.Close()
-/*
-			var wg2 sync.WaitGroup
-
-			wg2.Add(len(ClientIPs))
-			for _, v := range(ClientIPs) {
-				go func() {
-					outconn, err := net.Dial("tcp4", v+":6264")
-					if err != nil {
-						log.Println(err)
-						wg2.Done()
-						return
-					}
-
-					n, err := outconn.Write(buffer)
-					if err != nil {
-						log.Println("n:", n, err)
-					}
-					outconn.Close()
-					wg2.Done()
-				}()
-			}
-			wg2.Wait()
-*/
-		}
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Println(err)
 	}
-	wg.Done()
 }
